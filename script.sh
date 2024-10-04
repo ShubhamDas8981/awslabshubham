bc8f5050d1e0fde73647ae71a45c827ea852fb30
Shubham Das <113762822+ShubhamDas8981@users.noreply.github.com>
2024-10-04 20:21:45 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ systemctl enable nginx
 apt-get install -y git
 curl -SL https://deb.nodesource.com/setup_16.x|sudo -E bash -
 apt-get install -y nodejs
-git clone http://github.com/sudip7407/Repo1.git
+git clone https://github.com/ShubhamDas8981/awslabshubham.git
 cd Repo1
 npm install
 node index.j
